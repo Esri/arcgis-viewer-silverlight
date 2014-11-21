7fae5f599e8d294d9dc66271f535420d21702ada
Rich Zwaap <rzwaap@esri.com>
2014-11-21 10:58:02 -0500

    Fix for applying edits when related records are retrieved

--- src/AddIns/Tools/QueryRelatedRecords/QueryRelatedRecords.AddIns/QueryRelatedViewModel.cs
+++ src/AddIns/Tools/QueryRelatedRecords/QueryRelatedRecords.AddIns/QueryRelatedViewModel.cs
@@ -811,7 +811,6 @@ namespace QueryRelatedRecords.AddIns
                 {
                     //  graphic.Geometry.SpatialReference.WKID = map.SpatialReference.WKID;
                     list.Add((int)graphic.Attributes[oidField]);
-                    resultsLayer.Graphics.Add(graphic);
                 }
             }
 
